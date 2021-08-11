9c5df482f57a22b99f5e5473774d401dc3f592d3
Jenny Malmoe <jenny.c.borglin@gmail.com>
2021-08-11 11:06:13 +0000

    Changing Procfile configurations

--- app.py
+++ app.py
@@ -13,6 +13,6 @@ def test():
 # How and where to run app. 
 # Don't forget to change from True to False when submitting and deploying. 
 if __name__ == "__main__":
-    app.run(host=os.environ.get("IP"),
+    app.run(host=os.environ.get("0.0.0.0"),
             port=int(os.environ.get("PORT")),
             debug=True)
