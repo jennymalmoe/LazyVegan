bd1476e4d02832706e53c3deba0cd4d9d76546f5
Jenny Malmoe <jenny.c.borglin@gmail.com>
2021-08-17 11:58:00 +0000

    Trying create function for the route recipe

--- app.py
+++ app.py
@@ -139,6 +139,13 @@ def add_recipe():
     categories = mongo.db.categories.find().sort("category_name", 1)
     return render_template("add_recipe.html", categories=categories)
 
+#@app.route("/recipe/recipe_id>", methods=["GET", "POST"])
+#def recipe(recipe_id):
+
+    #recipe = mongo.db.tasks.find_one({"_id": ObjectId(recipe_id)})
+    #recipe = mongo.db.recipe.find().sort("recipe_name", 1)
+    #return render_template("recipe.html", recipe=recipe)
+
 
 # How and where to run app
 # Don't forget to change from True to False when submitting and deploying 
